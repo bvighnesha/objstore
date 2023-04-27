63daf2d734039b271fca40f8165a17cc288432ed
Vighnesha Bojja <vighnesha@Vighneshas-MBP.Optilink>
2023-04-27 12:16:35 +0530

    Add support for read from console

---
+++ console/console.go
@@ -0,0 +1,95 @@
+package console
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"objstore/db"
+	"objstore/obj"
+	"strings"
+)
+
+func Start(db *db.DB) {
+	fmt.Println("Please provide a command: SET, GET, LIST or DEL")
+	var command string
+	fmt.Scanln(&command)
+
+	switch command {
+	case "SET":
+		fmt.Println("Please provide a object to set")
+		var data string
+		fmt.Scanln(&data)
+
+		var object obj.Object
+
+		var person obj.Person
+		err := json.Unmarshal([]byte(data), &person)
+		if err != nil {
+			var animal obj.Animal
+			err := json.Unmarshal([]byte(data), &animal)
+			if err != nil {
+				fmt.Errorf("failed to unmarshal object: %w", err)
+			}
+			object = &animal
+
+		}
+		object = &person
+
+		err = db.Store(context.Background(), object)
+		if err != nil {
+			fmt.Println("Error storing object:", err)
+		}
+	case "GET":
+		fmt.Println("Do you want to get by name or ID?")
+		var kind string
+		fmt.Scanln(&kind)
+
+		switch strings.ToUpper(kind) {
+		case "NAME":
+			fmt.Println("Please provide a name to get")
+			var name string
+			fmt.Scanln(&name)
+			o, err := db.GetObjectByName(context.Background(), name)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Println(o)
+		case "ID":
+			fmt.Println("Please provide an ID to get")
+			var id string
+			fmt.Scanln(&id)
+			db.GetObjectByID(context.Background(), id)
+
+		default:
+			fmt.Println("Unknown command:", kind)
+		}
+
+	case "LIST":
+		fmt.Println("Please provide a kind to list")
+		var kind string
+		fmt.Scanln(&kind)
+		objs, err := db.ListObjects(context.Background(), kind)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		for _, obj := range objs {
+			fmt.Println(obj)
+		}
+
+	case "DEL":
+		fmt.Println("Please provide a id to delete")
+		var id string
+		fmt.Scanln(&id)
+		err := db.DeleteObject(context.Background(), id)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("Deleted ID %s\n", id)
+
+	default:
+		fmt.Println("Unknown command:", command)
+	}
+}
--- main.go
+++ main.go
@@ -1,31 +1,11 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	db2 "objstore/db"
-	obj2 "objstore/obj"
-	"time"
+	"objstore/console"
+	"objstore/db"
 )
 
 func main() {
-	fmt.Println("Hello World")
-	db := db2.NewObjectDB()
-	err := db.Store(context.Background(), &obj2.Person{
-		Name:      "test",
-		LastName:  "Bojja",
-		Birthday:  "04-11-1989",
-		BirthDate: time.Time{},
-	})
-
-	fmt.Println(err)
-
-	obj, err := db.ListObjects(context.Background(), "person")
-	fmt.Println("ListObjects", err)
-	fmt.Println(obj[0].GetName())
-	obj1, err := db.GetObjectByName(context.Background(), "test")
-	fmt.Println("GetObjectByName", err)
-	fmt.Println(obj1.GetName())
-	err = db.DeleteObject(context.Background(), obj[0].GetID())
-	fmt.Println("DeleteObject", err)
+	db := db.NewObjectDB()
+	console.Start(db)
 }
